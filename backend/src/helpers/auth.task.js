c385dae261e6924655f6528d3e63525c48b1098f
Unknown <zerpa2007@hotmail.com>
2021-07-10 10:26:57 -0400

    Delete and Update Task done
    not tested

--- backend/src/helpers/auth.task.js
+++ backend/src/helpers/auth.task.js
@@ -26,15 +26,50 @@ const createTask =async (req, res) => {
 
 const updateTask = async (req, res) => {
     const id = req.params.id;
+    const {title, description, tag, date} = req.body;
     try{
+       await db.query('BEGIN'); 
        const checkId = await db.query(queries.CHECKID, [id]);
-       
+
+       if(checkId.rows != ''){
+        const response = await db.query(queries.UPDATE_USER, [title, description, tag, date, id]);
+        console.log(response.rows);
+        res.status(200).send(`Task ${id} updated!`)
+        await db.query('COMMIT');
+       }else{
+        await db.query('ROLLBACK');
+        res.status(400).send(`Username ${id} not Found!`);
+       }
     }catch(err){
         await db.query('ROLLBACK');
         res.status(500).send('Server Error!');
         throw err;
     }
 }
+
+const deleteTask = async (req, res) => {
+    try{
+       const id = req.params.id;
+     
+       await db.query('BEGIN'); 
+       const checkId = await db.query(queries.CHECKID, [id]);
+
+       if(checkId =! '') {
+           const response = await db.query(queries.DELETE_TASK, [id]);
+           console.log(response.rows);
+           res.status(200).send(`Task ${id} deleted!`);
+            await db.query('COMMIT');
+       }else{
+        await db.query('ROLLBACK');
+        res.status(400).send(`Task ${id} not Found!`);
+       }
+    }catch(err){      
+        await db.query('ROLLBACK');
+        res.status(500).send('Server Error!');
+        throw err;
+    }
+}
 module.exports = {
-    createTask
+    createTask,
+    updateTask
 }
